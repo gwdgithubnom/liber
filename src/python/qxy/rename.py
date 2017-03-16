95fa0801ad6c5fa2649aa2654cf8a2642e78827a
qixiyu <1044788593@qq.com>
2017-03-16 13:13:22 +0800

    第一版

--- src/python/qxy/rename.py
+++ src/python/qxy/rename.py
@@ -1,13 +1,9 @@
 import os
 import logging
-import sys
+import re
 import configparser
 
-
-
-
-def rename_dir(url,static=True,reverse=True):
-    """"
+""""
     根据static的值进行文件夹自动重命名，命名规则
     YYYYMMDDhhmmsss+3[001]
     directory.ini
@@ -21,8 +17,28 @@ def rename_dir(url,static=True,reverse=True):
     建议对这里的工作进行定义多个子函数。定义的子函数请以_开头。
     在进行一些具体的操作，需要输出相关日志操作
     """""
-    """更改指定文件夹下的文件名"""
-    if _exist_(url):
+"""根据reverse进行反向目录生成"""
+def _reverse_(doc_name):
+
+    conf = configparser.ConfigParser()
+    conf.read("directory")
+    options = conf.options(doc_name)
+    if not os.path.exists(doc_name):
+        url = os.path.abspath(doc_name)#如果路径不存在，得到绝对路径
+    for option in options:
+        str_val = conf.get(doc_name,option )
+        New=os.path.join(doc_name,option);
+        old=os.path.join(doc_name,str_val);
+        print(old+"\n"+New)
+        os.rename(New,old);
+
+
+
+"""如果给定路径存在，更改指定文件夹下的文件名"""
+
+def rename_dir(url,static=True,reverse=True):
+
+    if _exist_(url)and reverse:
         if not os.path.exists(url):
             url = os.path.abspath(url)#如果路径不存在，得到绝对路径
         a = 0
@@ -35,22 +51,13 @@ def rename_dir(url,static=True,reverse=True):
             New=os.path.join(url,str(a)+filetype);#新的文件路径
             print(old+"\n"+New)
             os.rename(old, New);
-            _store_(old, a)
+            _store_(url,files, a)
             a=a+1
+    else:
+        _reverse_(url)
 
 
-
-"""""def rename():
-    path="../abc"#"这里替换为你的文件夹的路径";
-    filelist=os.listdir(path)#该文件夹下所有的文件（包括文件夹）
-    for files in filelist:#遍历所有文件
-        Olddir=os.path.join(path,files);#原来的文件路径
-        filename=os.path.splitext(files)[0];#文件名
-        filetype=os.path.splitext(files)[1];#文件扩展名
-        Newdir=os.path.join(path,"1"+filetype);#新的文件路径
-        os.renames(Olddir,Newdir);#重命名"""
-
-
+"""判断所给的路径是否存在"""
 def _exist_(url):
     """ s=os.path.abspath('../test')
     print(s)
@@ -65,19 +72,36 @@ def _exist_(url):
     else:
         print(url + " don't exist or isn't a dir")
 
+"""将更改后的文件oldname和newname以section的方式存到directory中"""
+def _store_(doc_name,files, a):
+    """
 
-def _store_(doc_name, a):
+    :param doc_name:
+    :param files:
+    :param a:
+    :return:
+    """
 
-    ftest = open('directory.ini','+a')
-    config_write = configparser.RawConfigParser()
-    config_write.add_section(str(a))
-    config_write.set(str(a),doc_name,str(a))
+    config_write = configparser.ConfigParser()
+    config_write.read("directory")
+    check=config_write.sections()
+    print("check:"+ str(check))
+    n=False
+    if doc_name in check:
+        n=True
+        config_write.set(doc_name,files,str(a))
+
+    if n==False:
+        config_write.add_section(doc_name)
+        config_write.set(doc_name,files,str(a))
+    ftest = open('directory','w+')
     config_write.write(ftest)
     ftest.close()
+    print(n)
 
 
 if __name__ == "__main__":
-    rename_dir(url="otest",static=True,reverse=True)
+    rename_dir(url="otest/abccc",static=True,reverse=False)
 
 
 
